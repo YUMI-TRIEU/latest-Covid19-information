ea437dc987c7680f93622b1b7960dd08c10ef229
Triệu Thị Thanh Hằng <yumitrieu@gmail.com>
2020-11-01 12:45:23 +0700

    Update covid19.test.js
    Adding dummy data

--- src/components/covid19.test.js
+++ src/components/covid19.test.js
@@ -22,7 +22,45 @@ describe('Covid19 component test', () => {
   });
 
   it('Fetches data from server when server returns a successful response', done => {
-    const mockSuccessResponse = {data: "datavalue"};
+    const mockSuccessResponse = {
+          "Message": "",
+          "Global": {
+            "NewConfirmed": 565744,
+            "TotalConfirmed": 45576990,
+            "NewDeaths": 7839,
+            "TotalDeaths": 1188787,
+            "NewRecovered": 254986,
+            "TotalRecovered": 30569006
+          },
+          "Countries": [{
+              "Country": "Afghanistan",
+              "CountryCode": "AF",
+              "Slug": "afghanistan",
+              "NewConfirmed": 66,
+              "TotalConfirmed": 41334,
+              "NewDeaths": 1,
+              "TotalDeaths": 1533,
+              "NewRecovered": 19,
+              "TotalRecovered": 34258,
+              "Date": "2020-11-01T02:32:03Z",
+              "Premium": {}
+            },
+            {
+              "Country": "Albania",
+              "CountryCode": "AL",
+              "Slug": "albania",
+              "NewConfirmed": 319,
+              "TotalConfirmed": 20634,
+              "NewDeaths": 3,
+              "TotalDeaths": 502,
+              "NewRecovered": 90,
+              "TotalRecovered": 11097,
+              "Date": "2020-11-01T02:32:03Z",
+              "Premium": {}
+            }
+          ],
+          "Date": "2020-11-01T02:32:03Z"
+        };
     const mockJsonPromise = Promise.resolve(mockSuccessResponse);
     const mockFetchPromise = Promise.resolve({
       json: () => mockJsonPromise,
@@ -35,12 +73,50 @@ describe('Covid19 component test', () => {
     expect(global.fetch).toHaveBeenCalledWith('https://api.covid19api.com/summary');
 
     process.nextTick(() => {
-
+//       console.log(wrapper.debug());
       // Expect state after fetches data successfully
       expect(wrapper.state()).toEqual({
         error: null,
         isLoaded: true,
-        covidApiData: {data : "datavalue"},
+        covidApiData: {
+          "Message": "",
+          "Global": {
+            "NewConfirmed": 565744,
+            "TotalConfirmed": 45576990,
+            "NewDeaths": 7839,
+            "TotalDeaths": 1188787,
+            "NewRecovered": 254986,
+            "TotalRecovered": 30569006
+          },
+          "Countries": [{
+              "Country": "Afghanistan",
+              "CountryCode": "AF",
+              "Slug": "afghanistan",
+              "NewConfirmed": 66,
+              "TotalConfirmed": 41334,
+              "NewDeaths": 1,
+              "TotalDeaths": 1533,
+              "NewRecovered": 19,
+              "TotalRecovered": 34258,
+              "Date": "2020-11-01T02:32:03Z",
+              "Premium": {}
+            },
+            {
+              "Country": "Albania",
+              "CountryCode": "AL",
+              "Slug": "albania",
+              "NewConfirmed": 319,
+              "TotalConfirmed": 20634,
+              "NewDeaths": 3,
+              "TotalDeaths": 502,
+              "NewRecovered": 90,
+              "TotalRecovered": 11097,
+              "Date": "2020-11-01T02:32:03Z",
+              "Premium": {}
+            }
+          ],
+          "Date": "2020-11-01T02:32:03Z"
+        },
         keyword: null
       }); 
       global.fetch.mockClear();
